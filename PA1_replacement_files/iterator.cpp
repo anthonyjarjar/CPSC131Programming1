c4fe4c1149a976def89f675e4db896fecafb6f82
anthonyjarjar <anthjar203@gmail.com>
2022-10-05 17:02:23 -0700

    added files

---
+++ PA1_replacement_files/iterator.cpp
@@ -0,0 +1,31 @@
+#include <iostream>     
+#include <iterator> 
+#include <vector>
+#include <list>  
+#include <array>
+
+int main (){
+ /*
+  int FindItem(std::vector<std::string>g , std::string groceryItem){
+    std::vector<std::string>::iterator it;
+    
+    it = find(g.begin() , g.end() , groceryItem); 
+
+    if(it == g.end()){
+      return g.size();
+    }
+
+    return it-g.begin();
+  }
+  */
+
+    std::cout << "This works" << std::endl;
+
+    void FindItem(){
+        
+    }
+
+
+    return 0;
+
+}
